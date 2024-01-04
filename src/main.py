20a817a43bf44d34399c868237002f66f009207d
Danil Sheen <dan.80956@gmail.com>
2024-01-04 17:25:01 +0300

    add tasks in main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from tasks.routers import router as task_router
 from employees.routers import router as employee_router
 
 app = FastAPI()
+app.include_router(task_router)
 app.include_router(employee_router)
 
 
@@ -13,6 +15,7 @@ def hello():
 @app.on_event("startup")
 def start():
     import database
+    from tasks.models import Task
     from employees.models import Employee
 
     database.Base.metadata.create_all(database.engine)
