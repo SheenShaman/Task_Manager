6132efa75ae1f048eb79f0e65635b21d541fd1ac
Danil Sheen <dan.80956@gmail.com>
2024-01-05 14:14:44 +0300

    fixed employees crud

--- src/employees/crud.py
+++ src/employees/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from employees.models import Employee
-from employees.schemas import EmployeeCreate
+from employees.schemas import EmployeeCreate, EmployeeUpdate
 
 
 def create_employee(db: Session, employee: EmployeeCreate):
@@ -21,15 +21,12 @@ def get_employee_by_id(db: Session, employee_id: int):
     return db.query(Employee).filter(Employee.id == employee_id).first()
 
 
-def update_employee(db: Session, employee_id: int, name: str, position: str):
-    new_employee = get_employee_by_id(db=db, employee_id=employee_id)
-
-    new_employee.name = name
-    new_employee.position = position
-
+def update_employee(db: Session, employee: Employee, employee_update: EmployeeUpdate):
+    for name, value in employee_update.model_dump().items():
+        setattr(employee, name, value)
     db.commit()
-    db.refresh(new_employee)
-    return new_employee
+    db.refresh(employee)
+    return employee
 
 
 def delete_employee(db: Session, employee_id: int):
