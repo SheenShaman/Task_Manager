89a947bdf03ef4967f6e8487e17f2e0d17f7fc3d
Danil Sheen <dan.80956@gmail.com>
2024-01-07 15:29:54 +0300

    fixes crud

--- src/crud/employees_crud.py
+++ src/crud/employees_crud.py
@@ -1,45 +1,49 @@
+from typing import List, Type, Union
 from sqlalchemy.orm import Session
 
-from src.models.employees_model import Employee
-from src.schemas.employees_schemas import EmployeeCreate, EmployeeUpdate
+from models import Employee
+from schemas.employees_schemas import EmployeeCreate, EmployeeUpdate
 
 
 class EmployeeCRUD:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_employee(self, employee_schema: EmployeeCreate):
-        """ Create Employee"""
-        is_busy_value = employee_schema.is_busy if employee_schema.is_busy is not None else False
-
-        new_employee = Employee(name=employee_schema.name,
-                                position=employee_schema.position,
-                                is_busy=is_busy_value)
-
+    def create_employee(self, employee_schema: EmployeeCreate) -> Employee:
+        """ Create Employee """
+        # is_busy_value = employee_schema.is_busy if employee_schema.is_busy is not None else False
+        #
+        # new_employee = Employee(name=employee_schema.name,
+        #                         position=employee_schema.position,
+        #                         is_busy=is_busy_value)
+        new_employee = Employee(**employee_schema.model_dump(exclude_unset=True))
         self.db.add(new_employee)
         self.db.commit()
         self.db.refresh(new_employee)
         return new_employee
 
-    def get_employees(self, skip: int = 0, limit: int = 100):
-        """ Get Employees"""
+    def get_employees(self, skip: int = 0, limit: int = 100) -> List[Type[Employee]]:
+        """ Get Employees """
         return self.db.query(Employee).offset(skip).limit(limit).all()
 
-    def get_employee_by_id(self, employee_id: int):
-        """ Get Employee by id"""
-        return self.db.query(Employee).filter(Employee.id == employee_id).first()
+    def get_employee_by_id(self, employee_id: int) -> Union[Employee, None]:
+        """ Get Employee by id """
+        return self.db.query(Employee).filter_by(id=employee_id).first()
 
-    def update_employee(self, employee_id: int, employee_schema: EmployeeUpdate):
-        """ Update Employee"""
+    def update_employee(self, employee_id: int, employee_schema: EmployeeUpdate) -> Union[Employee, None]:
+        """ Update Employee """
         updated_employee = self.get_employee_by_id(employee_id=employee_id)
-        for name, value in employee_schema.model_dump().items():
-            setattr(updated_employee, name, value)
-        self.db.commit()
-        self.db.refresh(updated_employee)
+        if updated_employee:
+            for field, value in employee_schema.model_dump(exclude_unset=True).items():
+                setattr(updated_employee, field, value)
+            self.db.commit()
+            self.db.refresh(updated_employee)
         return updated_employee
 
-    def delete_employee(self, employee_id: int):
-        """ Delete Employee"""
+    def delete_employee(self, employee_id: int) -> Union[Employee, None]:
+        """ Delete Employee """
         employee = self.get_employee_by_id(employee_id=employee_id)
-        self.db.delete(employee)
-        self.db.commit()
+        if employee:
+            self.db.delete(employee)
+            self.db.commit()
+        return employee
--- src/crud/tasks_crud.py
+++ src/crud/tasks_crud.py
@@ -1,42 +1,44 @@
+from typing import List, Type, Union
 from sqlalchemy.orm import Session
 
-from src.models.tasks_model import Task
-from src.schemas.tasks_schemas import TaskCreate, TaskUpdate
+from models import Task
+from schemas.tasks_schemas import TaskCreate, TaskUpdate
 
 
-def create_task(db: Session, task: TaskCreate):
-    # new_task = Task(**task.model_dump())
-    new_task = Task(
-        name=task.name,
-        is_parent_task=task.is_parent_task,
-        deadline=task.deadline,
-        status=task.status,
-        executor_id=task.executor_id,
-        parent_task_id=task.parent_task_id,
-    )
-    db.add(new_task)
-    db.commit()
-    db.refresh(new_task)
-    return new_task
+class TaskCRUD:
+    def __init__(self, db: Session):
+        self.db = db
 
+    def create_task(self, task_schema: TaskCreate) -> Task:
+        """ Create Task """
+        new_task = Task(**task_schema.model_dump(exclude_unset=True))
+        self.db.add(new_task)
+        self.db.commit()
+        self.db.refresh(new_task)
+        return new_task
 
-def get_tasks(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Task).offset(skip).limit(limit).all()
+    def get_tasks(self, skip: int = 0, limit: int = 100) -> List[Type[Task]]:
+        """ Get Tasks """
+        return self.db.query(Task).offset(skip).limit(limit).all()
 
+    def get_task_by_id(self, task_id: int) -> Union[Task, None]:
+        """ Get Task by id """
+        return self.db.query(Task).filter_by(id=task_id).first()
 
-def get_task_by_id(db: Session, task_id: int):
-    return db.query(Task).filter(Task.id == task_id).first()
+    def update_task(self, task_id: int, task_schema: TaskUpdate) -> Union[Task, None]:
+        """ Update Task """
+        updated_task = self.get_task_by_id(task_id=task_id)
+        if updated_task:
+            for field, value in task_schema.model_dump(exclude_unset=True).items():
+                setattr(updated_task, field, value)
+            self.db.commit()
+            self.db.refresh(updated_task)
+        return updated_task
 
-
-def update_task(db: Session, task: Task, task_update: TaskUpdate):
-    for name, value in task_update.model_dump().items():
-        setattr(task, name, value)
-    db.commit()
-    db.refresh(task)
-    return task
-
-
-def delete_task(db: Session, task_id: int):
-    task = get_task_by_id(db=db, task_id=task_id)
-    db.delete(task)
-    db.commit()
+    def delete_task(self, task_id: int) -> Union[Task, None]:
+        """ Delete Task """
+        task = self.get_task_by_id(task_id=task_id)
+        if task:
+            self.db.delete(task)
+            self.db.commit()
+        return task
